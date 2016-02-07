9a96b5a24d94ac7e85240803d36af94c6e5e2bb4
Nate Finch <finchproservices@gmail.com>
2016-02-07 15:28:49 +0900

    grunt concat js and css working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,34 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      files: ['Gruntfile.js', 'js/custom.js'],
       options: {
         globals: {
           jQuery: true
         }
       }
     },
+
+
+    concat: {
+      options: {
+        //banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        //'<%= grunt.template.today("yyyy-mm-dd") %> */',
+        //separator: '/*-----------------------------------------*/',
+      },
+      javascript: {
+        src: ['js/*.js'],
+        dest: 'built.js'
+      },
+      css: {
+        src: ['css/main-style.css', 'css/bootstrap.min.css', 'css/bootstrap-theme.min.css', 'css/bootstrap-theme.min.css', 'css/media-queries.css', 'css/custom.css'],
+        dest: 'style.css'
+      }
+    },
+
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
@@ -17,7 +37,8 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'concat',]);
 
 };
